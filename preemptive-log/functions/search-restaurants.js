45a926232d83a1e4cfeb332d15de1fdd84db23da
theburningmonk <theburningmonk@gmail.com>
2020-08-17 16:11:49 +0200

    feat(preemptive): added preemptive-log example

---
+++ preemptive-log/functions/search-restaurants.js
@@ -0,0 +1,51 @@
+const DocumentClient = require('aws-sdk/clients/dynamodb').DocumentClient
+const dynamodb = new DocumentClient()
+const ssm = require('@middy/ssm')
+const failureLambda = require('failure-lambda')
+const Log = require('@dazn/lambda-powertools-logger')
+const wrap = require('@dazn/lambda-powertools-pattern-basic')
+
+const { serviceName, stage } = process.env
+
+const tableName = process.env.restaurants_table
+
+const findRestaurantsByTheme = async (theme, count) => {
+  const req = {
+    TableName: tableName,
+    Limit: count,
+    FilterExpression: "contains(themes, :theme)",
+    ExpressionAttributeValues: { ":theme": theme }
+  }
+
+  if (theme) {
+    req.FilterExpression = "contains(themes, :theme)"
+    req.ExpressionAttributeValues = { ":theme": theme }
+  }
+
+  const resp = await dynamodb.scan(req).promise()
+  return resp.Items
+}
+
+const handler = failureLambda(async (event, context) => {
+  const req = JSON.parse(event.body)
+  const theme = req.theme
+  const restaurants = await findRestaurantsByTheme(theme, process.env.defaultResults)
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(restaurants)
+  }
+
+  return response
+})
+
+module.exports.handler = wrap(handler).use(ssm({
+  cache: true,
+  cacheExpiryInMillis: 5 * 60 * 1000, // 5 mins
+  names: {
+    config: `/${serviceName}/${stage}/search-restaurants/config`
+  },
+  onChange: () => {
+    const config = JSON.parse(process.env.config)
+    process.env.defaultResults = config.defaultResults
+  }
+}))
